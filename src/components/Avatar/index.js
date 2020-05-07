221a25e9c754a91ef4fe4d6b944841a1c5c8b35b
Denner Rondinely <denner.rondinely@gmail.com>
2020-05-06 23:21:36 -0300

    trabalhando com imagens

---
+++ src/components/Avatar/index.js
@@ -0,0 +1,22 @@
+import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+import Img from "gatsby-image"
+
+// import { Container } from './styles';
+
+function Avatar() {
+  const { avatarImage } = useStaticQuery(graphql`
+  query {
+    avatarImage: file(relativePath: { eq: "profile.jpeg" }) {
+      childImageSharp {
+        fixed(width: 60, height: 60) {
+          ...GatsbyImageSharpFixed
+        }
+      }
+    }
+  }
+  `)
+  return <Img fixed={avatarImage.childImageSharp.fixed} />
+}
+
+export default Avatar

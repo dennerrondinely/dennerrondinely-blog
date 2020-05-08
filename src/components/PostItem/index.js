e0c461363711b7b1aaaaac0a81bc2fac3a6383cd
Denner Rondinely <denner.rondinely@gmail.com>
2020-05-07 23:53:13 -0300

    Criar post item

---
+++ src/components/PostItem/index.js
@@ -0,0 +1,33 @@
+import React from "react"
+import PropTypes from 'prop-types'
+
+import * as S from "./styles"
+
+function PostItem({ slug, category, date, timeToRead, title, description, background }) {
+  return (
+    <S.PostItemLink to={slug}>
+      <S.PostItemWrapper>
+        <S.PostItemTag background={background}>{category}</S.PostItemTag>
+        <S.PostItemInfo>
+          <S.PostItemDate>
+            {date} . {timeToRead} min de leitura
+          </S.PostItemDate>
+          <S.PostItemTitle>{title}</S.PostItemTitle>
+          <S.PostItemDescription>{description}</S.PostItemDescription>
+        </S.PostItemInfo>
+      </S.PostItemWrapper>
+    </S.PostItemLink>
+  )
+}
+
+PostItem.propTypes = {
+  slug: PropTypes.string.isRequired,
+  category: PropTypes.string.isRequired,
+  date: PropTypes.string.isRequired,
+  timeToRead: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  background: PropTypes.string,
+}
+
+export default PostItem

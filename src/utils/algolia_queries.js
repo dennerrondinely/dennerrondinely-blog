0af616767c3599addb5eee87dc3e028c5bb3804a
Denner Rondinely <denner.rondinely@gmail.com>
2020-05-11 21:15:41 -0300

    configurações do algolia

---
+++ src/utils/algolia_queries.js
@@ -0,0 +1,42 @@
+const postsQuery = `{
+  posts: allMarkdownRemark(
+    sort: { fields: frontmatter___date, order: DESC }
+  ) {
+    edges {
+      node {
+        objectID: id
+        fields {
+          slug
+        }
+        frontmatter {
+          title
+          description
+          date(locale: "pt-br", formatString: "DD [de] MMMM [de] YYYY")
+          category
+        }
+        excerpt(pruneLenght: 5000)
+      }
+    }
+  }
+}`
+
+const flatten = arr =>
+  arr.map(({ node: { frontmatter, ...rest } }) => ({
+    ...frontmatter,
+    date_timestamp: parseInt(
+      (new Date(frontmatter.date_timestamp).getTime() / 1000).toFixed(0)
+    ),
+    ...rest,
+  }))
+const settings = { attributesToSnippet: [`excerpt:20`] }
+
+const queries = [
+  {
+    query: postsQuery,
+    transformer: ({ data }) => flatten(data.posts.edges),
+    indexName: `Posts`,
+    settings,
+  },
+]
+
+module.exports = queries

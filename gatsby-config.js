416f3d19520ad5da3ef829f5faa0439fc614f959
Denner Rondinely <denner.rondinely@gmail.com>
2020-05-15 00:05:38 -0300

    adicionando icone

--- gatsby-config.js
+++ gatsby-config.js
@@ -79,7 +79,7 @@ module.exports = {
         background_color: `#16202c`,
         theme_color: `#16202c`,
         display: `fullscreen`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `static/assets/img/denner-icon.png`, // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-sitemap`,
